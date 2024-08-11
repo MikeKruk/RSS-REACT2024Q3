13adbf13a8cb9988a535c906551019cc2eaa86d8
Mikhail Kruk <mi.kruk95@gmail.com>
2024-08-11 23:51:47 +0200

    third commit

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,6 +11,8 @@ const Card: React.FC<ICardProps> = ({
 }: ICardProps) => {
   const handleClick = () => (onClick ? onClick() : null);
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('git');
+
     if (onSelect) {
       onSelect(event.target.checked);
     }
